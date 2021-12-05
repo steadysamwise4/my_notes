1b381af2d1c51bd5f75975b212232641b31ea219
Sam Davenport <samueldavenport@att.net>
2021-12-05 11:26:48 -0600

    after much difficulty, I think my problem was pathing, app now functions to get notes, create notes, and delete notes

--- Routes/api.js
+++ Routes/api.js
@@ -1,26 +1,32 @@
 const router = require('express').Router();
-const notesArray = require('../db.json');
+const notesArray = require('../db/db.json');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-router.get('/api/notes', (req, res) => {
-    try {
-        res.json(notesArray)
-        
-    } catch (error) {
-       res.json(error) 
-    }
+const path = require('path');
+
+
+router.get('/api/notes', (req, res) => {  
+    res.sendFile(path.join(__dirname, '../db/db.json'));  
+        // fs.readFileSync(path.join(__dirname, notesArray), "utf-8", (err, data) => {
+        //     if (err) {
+        //         res.status(500).end
+        //     }
+        // console.log(data);
+        // });
 });
 
 router.post('/api/notes', (req, res) => {
-    const newNote = {title: req.body.title, text: req.body.text, id: uuidv4()}
-    try {
-        notesArray.push(newNote).then(newArray => {
-            fs.writeFileSync("../db.json", newArray);
-            res.json({ msg: 'success' })
-        })
-    } catch (error) {
-        res.json(error);
-    }
-})
+    const notesArray = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    notesArray.push(newNote);
+    fs.writeFileSync("../db/db.json", JSON.stringify(notesArray))
+        res.json(notesArray);
+    });
+
+       
+   
+    
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notesArray = [];
+const db = require('./db/db.json');
+
 // Helper method for generating unique ids
 // const uuid = require('./helpers/uuid');
 
 const htmlRoutes = require('./Routes/html');
-const apiRoutes = require('./Routes/api');
+// const apiRoutes = require('./Routes/api');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -16,9 +19,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(htmlRoutes);
-app.use(apiRoutes);
+// app.use(apiRoutes);
 
+app.get('/api/notes', (req, res) => {
+    const notesArray = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+    console.log(notesArray);
+    res.send(notesArray);
+});
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.post('/api/notes', (req, res) => {
+    const notesArray = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json")));
+    const newNote = req.body;
+    newNote.id = (notesArray.length + 1).toString();
+    notesArray.push(newNote);
+    fs.writeFileSync(path.join(__dirname,"./db/db.json"), JSON.stringify(notesArray))
+        res.json(notesArray);
+    });
+
+app.delete('/api/notes/:id', (req, res) => {
+    const notesArray = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json")));
+    const deleteNote = notesArray.filter((noteBgon) => noteBgon.id !== req.params.id)
+    fs.writeFileSync(path.join(__dirname,"./db/db.json"), JSON.stringify(deleteNote))
+        res.json(deleteNote);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
